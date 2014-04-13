6e3b1da7c6bd00877b6ebe3484698fa63b7387f1
Stephan Hochhaus <stephan@yauh.de>
2014-04-13 18:14:07 +0200

    ignore certain people
    don’t retweet everyone you find…

--- retweetbot.js
+++ retweetbot.js
@@ -5,19 +5,19 @@ if (Meteor.isServer) {
     var hashtag = 'meteorjob'; // these are the droids we're looking for
     var checkInterval = 60000; // how much time to wait between checks
     var lastTweetId = 452841670879748100; // the birth of the bot
-    
+
     // remember the last tweet you actually processed and stored in mongo
     if (ProcessedTweets.find().count()) {
         lastTweetId = ProcessedTweets.findOne({}, {sort: {lastTweetId: -1}}).lastTweetId;
     };
-    
+
     // hey, use your own Twitter application https://apps.twitter.com
     // make sure to give read + write permissions before creating the token!!
-    Twit = new TwitMaker({ 
-        consumer_key: 'API Key',
-        consumer_secret: 'API secrect',
-        access_token: 'Token ID',
-        access_token_secret: 'Token Secret'
+    Twit = new TwitMaker({
+        consumer_key: '',
+        consumer_secret: '',
+        access_token: '',
+        access_token_secret: ''
     });
     console.log('Getting to work with Id ' + lastTweetId);
 
@@ -41,10 +41,17 @@ if (Meteor.isServer) {
                 console.log('Found ' + tweets.length + ' new tweets since Id ' + id);
                 for (var i = 0; i < tweets.length; i++) {
                     var retweetRegex = /^RT/;
+                    var ignored_users = ["meteorjobs"];
 
                     // don't care about Retweets
                     if (retweetRegex.test(tweets[i].text)) {
                         console.log('skipping a retweet by ' + tweets[i].user.screen_name);
+                        continue;
+                    }
+
+                    // don't retweet these people
+                    if (ignored_users.indexOf(tweets[i].user.screen_name) > -1 ) {
+                        console.log('ignoring a tweet by ' + tweets[i].user.screen_name);
                         continue;
                     }
 
@@ -55,12 +62,13 @@ if (Meteor.isServer) {
                     if (tweetThis.length > 140) {
                         var tweetThis = tweetThis.substring(0, 137) + '...';
                     }
-                    
+
                     // for safety reasons you need to uncomment the Twit.post and closing brackets
                     //Twit.post('statuses/update', { status: tweetThis }, function(err, reply) {
                         console.log('I tweet this: ' + tweetThis);
+                        id = parseInt(id)+1;
                     //})
-                    
+
                     if (parseInt(tweets[i].id) > parseInt(lastTweetId)) {
                         lastTweetId = tweets[i].id;
                         Fiber(function () {
